e2fd7ae9ee270c377762f46f9249fdd3721647a1
Vincent <vincent.loignon@mymail.champlain.edu>
2016-02-25 13:46:52 -0500

    - Created batch files to automate some of the process - Moved the dependencies for the executable to a deps folder - Added a default mp3

---
+++ CreateExecutable.bat
@@ -0,0 +1,2 @@
+pyinstaller --onefile StyroBotPy.py
+xcopy "%CD%\deps" "%CD%\dist" /E 
---
+++ InstallDependencies.bat
@@ -0,0 +1,13 @@
+@echo off
+
+echo "Installing Dependencies for StyroBotPy"
+echo "Installing Discord.py"
+pip install git+https://github.com/Rapptz/discord.py@async
+echo "Complete!"
+echo "Installing Cleverbot"
+pip install cleverbot
+echo "Complete!"
+echo "Installing Pafy"
+pip install pafy
+echo "Complete"
+echo "All Dependencies have been in Installed!"
