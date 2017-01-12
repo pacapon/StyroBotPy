1ae562817c328b378040a541ecf36b19c8ff1cf0
Vincent Loignon <vincent.loignon@gmail.com>
2017-01-12 01:09:55 -0500

    [Fix Issue #15][Cleverbot] - You can now use a mention with the bot to talk with cleverbot. This is equivalent to using the chat command

--- styrobot/plugins/cleverbotchat.py
+++ styrobot/plugins/cleverbotchat.py
@@ -3,6 +3,7 @@ import cleverbot
 import logging
 import styrobot
 import commands
+import re
 
 class CleverBotChat(Plugin):
     async def initialize(self, bot):
@@ -14,7 +15,19 @@ class CleverBotChat(Plugin):
         return True
 
     async def readMessage(self, message):
-        pass
+        if message.server.me in message.mentions: 
+            if len(message.mentions) == 1:
+                self.logger.debug('[readMessage]: Mention of bot is found. Parsing all text after mention in message and sending to cleverbot.')
+
+                botMessage = message.content.split('<@', 1)[1]
+                botMessage = botMessage.split('>', 1)[1]
+
+                botMessage = self.scrubMessage(botMessage)
+                self.logger.debug('[readMessage]: Message content: %s', botMessage)
+
+                await self._chat_(message.server, message.channel, message.author, botMessage)
+            else:
+                self.logger.debug('[readMessage]: There is more than one mention. Ignoring due to complexity.')
 
     @styrobot.plugincommand('Sends a message to CleverBot', name='chat', parserType=commands.ParamParserType.ALL)
     async def _chat_(self, server, channel, author, text):
@@ -22,3 +35,11 @@ class CleverBotChat(Plugin):
         response = self.cb.ask(text)
         self.logger.debug('[chat] [cleverbot]: %s', response)
         await self.bot.send_message(channel, response)
+
+    # Takes in the message's content and removes all markdown
+    def scrubMessage(self, message):
+        scrubbedMessage = re.findall('[^_*\-\'\"\`\~,]', message)
+        self.logger.debug('Scrubbed message: %s', ''.join(scrubbedMessage).lower())
+
+        return ''.join(scrubbedMessage).lower()
+
