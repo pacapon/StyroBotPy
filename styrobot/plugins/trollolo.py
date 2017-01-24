688e2d138feab7ad1e44faff62731d08890e8ae9
Vincent Loignon <vincent.loignon@gmail.com>
2017-01-23 23:09:42 -0500

    [Update][Trollolo Plugin] - Added some new commands to the trollolo plugin for lulz

--- styrobot/plugins/trollolo.py
+++ styrobot/plugins/trollolo.py
@@ -73,6 +73,65 @@ class Trollolo(Plugin):
         url = 'https://www.youtube.com/watch?v=QH2-TGUlwu4'
         await self.playTroll(server, channel, url, 'nyan')
 
+    @styrobot.plugincommand('*queues sax music*  We are number one! Hey!\nAvailable versions: original, runescape, synthwave, soviet', name='numberone')
+    async def _numberone_(self, server, channel, author, version):
+        url = ''
+        name = ''
+
+        if version == '' or version.lower() == 'original':
+            url = 'https://www.youtube.com/watch?v=PfYnvDL0Qcw'
+            name = 'numberone'
+        elif version.lower() == 'runescape':
+            url = 'https://www.youtube.com/watch?v=s0H5PgnC-iA'
+            name = 'numberone_runescape'
+        elif version.lower() == 'synthwave':
+            url = 'https://www.youtube.com/watch?v=7FkpM4FWa8A'
+            name = 'numberone_synthwave'
+        elif version.lower() == 'soviet':
+            url = 'https://www.youtube.com/watch?v=yCrYvHBMGLY'
+            name = 'numberone_soviet'
+        else:
+            self.logger.debug('[numberone]: The version you provided is invalid.')
+            await self.bot.send_message(channel, 'The version you provided is invalid.')
+            return
+
+        await self.playTroll(server, channel, url, name)
+
+    @styrobot.plugincommand('Yar har fiddle dee dee, being a pirate is alright with me! Do what you want cause a pirate is free, you are a pirate!', name='pirate')
+    async def _pirate_(self, server, channel, author):
+        url = 'https://www.youtube.com/watch?v=i8ju_10NkGY'
+        await self.playTroll(server, channel, url, 'pirate')
+
+    @styrobot.plugincommand('Yeeeeeeeeeeeee', name='yeee')
+    async def _yeee_(self, server, channel, author):
+        url = 'https://www.youtube.com/watch?v=q6EoRBvdVPQ'
+        await self.playTroll(server, channel, url, 'yeee')
+
+    @styrobot.plugincommand('Impressive... Most impressive.', name='impressive')
+    async def _impressive_(self, server, channel, author):
+        url = 'https://www.youtube.com/watch?v=bTlXZf5qsZI'
+        await self.playTroll(server, channel, url, 'impressive')
+
+    @styrobot.plugincommand('Power!!!!! **UNLIMITED POWER!!!!**', name='power')
+    async def _power_(self, server, channel, author):
+        url = 'https://www.youtube.com/watch?v=0D8i8QGgz0k'
+        await self.playTroll(server, channel, url, 'power')
+
+    @styrobot.plugincommand('Goood anakin, goooodd', name='goodanakin')
+    async def _goodanakin_(self, server, channel, author):
+        url = 'https://www.youtube.com/watch?v=VdFoAi_f30Q'
+        await self.playTroll(server, channel, url, 'goodanakin')
+
+    @styrobot.plugincommand('If the baggins loses, we eats it whole!', name='eatsitwhole')
+    async def _eatsitwhole_(self, server, channel, author):
+        url = 'https://www.youtube.com/watch?v=rV73aShBFgs'
+        await self.playTroll(server, channel, url, 'eatsitwhole')
+
+    @styrobot.plugincommand('We\'ve had one, yes, but what about second breakfast?', name='secondbreakfast')
+    async def _secondbreakfast_(self, server, channel, author):
+        url = 'https://www.youtube.com/watch?v=XkzvHtjnNOs'
+        await self.playTroll(server, channel, url, 'secondbreakfast')
+
     def dl_song(self, url, name):
         video = pafy.new(url)
         audio = video.audiostreams
